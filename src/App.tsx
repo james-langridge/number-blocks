d19795caf65eaa9bddb0352670040682afa52cb0
James <james.d.langridge@gmail.com>
2023-03-05 11:13:01 +0100

    Add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,70 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
 function generateShuffledNumArr({numSquares = 8}) {
-    const numbers = [];
+  const numbers = []
 
-    for (let i = 0; i < numSquares; i++) {
-        numbers.push(i+1);
-    }
+  for (let i = 0; i < numSquares; i++) {
+    numbers.push(i + 1)
+  }
 
-    for (let i = numbers.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-    }
+  for (let i = numbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[numbers[i], numbers[j]] = [numbers[j], numbers[i]]
+  }
 
-    return numbers
+  return numbers
 }
 
 function App() {
-    const [currentNum, setCurrentNum] = useState(1);
-    const [shuffledNumbers, setShuffledNumbers] = useState(() => generateShuffledNumArr({}))
+  const [currentNum, setCurrentNum] = useState(1)
+  const [shuffledNumbers, setShuffledNumbers] = useState(() =>
+    generateShuffledNumArr({}),
+  )
 
-    function clickHandler(event: React.MouseEvent) {
-        const clickedNum = Number(event.currentTarget.textContent)
+  function clickHandler(event: React.MouseEvent | React.KeyboardEvent) {
+    const clickedNum = Number(event.currentTarget.textContent)
 
-        if (clickedNum < currentNum) {
-            return
-        }
-
-        if (clickedNum === currentNum) {
-            document.getElementById(`square-${clickedNum}`)?.classList.add('green')
-
-            if (currentNum === shuffledNumbers.length) {
-                setCurrentNum(1)
-                shuffledNumbers.forEach((val, idx) => {
-                    document.getElementById(`square-${val}`)?.classList.remove('green')
-                })
-                setShuffledNumbers(generateShuffledNumArr({}))
-
-                return
-            }
-
-            setCurrentNum(() => currentNum + 1)
-        }
+    if (clickedNum < currentNum) {
+      return
     }
 
-    return (
+    if (clickedNum === currentNum) {
+      document.getElementById(`square-${clickedNum}`)?.classList.add('green')
+
+      if (currentNum === shuffledNumbers.length) {
+        setCurrentNum(1)
+        shuffledNumbers.forEach(val => {
+          document.getElementById(`square-${val}`)?.classList.remove('green')
+        })
+        setShuffledNumbers(generateShuffledNumArr({}))
+
+        return
+      }
+
+      setCurrentNum(() => currentNum + 1)
+    }
+  }
+
+  return (
     <div className="App">
       <div className="grid-container">
-          {shuffledNumbers.map(val => <div id={`square-${val}`} key={val} className="grid-item" onClick={clickHandler}>{val}</div>)}
+        {shuffledNumbers.map((val, idx) => (
+          <div
+            role={'button'}
+            tabIndex={idx}
+            id={`square-${val}`}
+            key={val}
+            className="grid-item"
+            onClick={clickHandler}
+            onKeyDown={clickHandler}
+          >
+            {val}
+          </div>
+        ))}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
