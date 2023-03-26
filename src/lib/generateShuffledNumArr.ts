f9554346b3fbecf464e10b51609f476a27d155e6
James <james.d.langridge@gmail.com>
2023-03-26 16:49:41 +0200

    Improve shuffle function

--- src/lib/generateShuffledNumArr.ts
+++ src/lib/generateShuffledNumArr.ts
@@ -1,10 +1,7 @@
 export function generateShuffledNumArr(numSquares = 8) {
-  const numbers = []
+  const numbers = Array.from({length: numSquares}, (_, i) => i + 1)
 
-  for (let i = 0; i < numSquares; i++) {
-    numbers.push(i + 1)
-  }
-
+  // Fisher-Yates (Knuth) shuffle algorithm
   for (let i = numbers.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
     ;[numbers[i], numbers[j]] = [numbers[j], numbers[i]]
